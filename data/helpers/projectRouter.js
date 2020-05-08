b6e9172f845c78da2631404759f898f28bceb695
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-05-08 09:57:49 -0700

    Created and tested all CRUD operations on the projectRouter.js

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -92,11 +92,49 @@ router.post("/:id/actions", validateProjectId, validateAction, (req, res) => {
 })
 
 router.delete("/:id", validateProjectId, (req, res) => {
+    const { id } = req.params;
 
+    Projects.remove(id)
+        .then(count => {
+            if(count){
+                res.status(204).end()
+            } else {
+                res.status(404).json({ message: "The project withe the specified ID was not found." })
+            }
+        })
+        .catch(error => {
+            console.log({ error })
+            res.status(500).json({ message: "Error deleting project with the specified ID." })
+        })
 })
 
 router.put("/:id", validateProjectId, validateProject, (req, res) => {
+    const { id } = req.params;
+    const { name, description } = req.body;
 
+    Projects.update(id, { name, description })
+        .then(user => {
+            if(user){
+                Projects.get(id)
+                    .then(updatedProject => {
+                        if(updatedProject){
+                            res.status(200).json(updatedProject)
+                        } else {
+                            res.status(404).json({ message: "Updated project was not found." })
+                        }
+                    })
+                    .catch(error => {
+                        console.log({ error })
+                        res.status(500).json({ message: "Error finding updated project." })
+                    })
+            } else {
+                res.status(404).json({ message: "Project with specified ID was not found." })
+            }
+        })
+        .catch(error => {
+            console.log({ error })
+            res.status(500).json({ message: "Error updating the project with specified ID." })
+        })
 })
 
 // Custom Middleware //
