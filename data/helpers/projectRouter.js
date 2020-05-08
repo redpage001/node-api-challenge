4ae897d37c8e0031ab54a29f232e3da96c50c23b
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-05-08 09:42:29 -0700

    Created middleware for project router and tested them.

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -5,23 +5,90 @@ const Actions = require('./actionModel');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-
+    Projects.get()
+        .then(projects => {
+            if(projects){
+                res.status(200).json(projects)
+            } else {
+                res.status(404).json({ message: "List of projects can not be found." })
+            }
+        })
+        .catch(error => {
+            console.log({ error })
+            res.status(500).json({ message: "Error retrieving projects." })
+        })
 })
 
 router.get("/:id", validateProjectId, (req, res) => {
-
+    res.status(200).json(req.project)
 })
 
 router.get("/:id/actions", validateProjectId, (req, res) => {
+    const { id } = req.params;
 
+    Projects.getProjectActions(id)
+        .then(actions => {
+            console.log(actions)
+            if(actions.length > 0){
+                res.status(200).json(actions)
+            } else {
+                res.status(404).json({ message: "The specified project has no actions." })
+            }
+        })
+        .catch(error => {
+            console.log({ error })
+            res.status(500).json({ message: "Error retrieving actions from project with specified ID." })
+        })
 })
 
 router.post("/", validateProject, (req, res) => {
+    const { name, description } = req.body;
 
+    Projects.insert({ name, description })
+        .then(project => {
+            Projects.get(project.id)
+                .then(newProject => {
+                    if(newProject){
+                        res.status(201).json(newProject)
+                    } else {
+                        res.status(404).json({ message: "The newly created project was not found." })
+                    }
+                })
+                .catch(error => {
+                    console.log({ error })
+                    res.status(500).json({ message: "Error in retrieving newly created project." })
+                })
+        })
+        .catch(error => {
+            console.log({ error })
+            res.status(500).json({ message: "Error creating project." })
+        })
 })
 
 router.post("/:id/actions", validateProjectId, validateAction, (req, res) => {
+    const { id: project_id } = req.project;
+    const { description, notes } = req.body;
+    console.log(req.project);
 
+    Actions.insert({ description, notes, project_id })
+        .then(action => {
+            Actions.get(action.id)
+                .then(newAction => {
+                    if(newAction){
+                        res.status(201).json(newAction)
+                    } else {
+                        res.status(404).json({ message: "The newly created action was not found." })
+                    }
+                })
+                .catch(error => {
+                    console.log({ error })
+                    res.status(500).json({ message: "Error retrieving newly created action." })
+                })
+        })
+        .catch(error => {
+            console.log({ error })
+            res.status(500).json({ message: "Error in adding action to project with specified ID." })
+        })
 })
 
 router.delete("/:id", validateProjectId, (req, res) => {
@@ -29,21 +96,53 @@ router.delete("/:id", validateProjectId, (req, res) => {
 })
 
 router.put("/:id", validateProjectId, validateProject, (req, res) => {
-    
+
 })
 
 // Custom Middleware //
 
 function validateProjectId(req, res, next) {
+    const { id } = req.params;
 
+    Projects.get(id)
+        .then(project => {
+            if(project){
+                req.project = project;
+                next();
+            } else {
+                res.status(404).json({ message: "Project with specified ID not found." })
+            }
+        })
+        .catch(error => {
+            console.log({ error })
+            res.status(500).json({ message: "Error retriving project with specfied ID." })
+        })
 }
 
 function validateProject(req, res, next) {
+    const { name, description } = req.body;
 
+    if(Object.entries(req.body).length === 0){
+        res.status(400).json({ message: "No project data was found." })
+    } else if (!name || !description ){
+        res.status(400).json({ message: "Name and Description fields are required." })
+    } else {
+        next();
+    }
 }
 
 function  validateAction(req, res, next) {
+    const { description, notes } = req.body;
 
+    if(Object.entries(req.body).length === 0){
+        res.status(400).json({ message: "No project data was found." })
+    } else if (!description || !notes ){
+        res.status(400).json({ message: "Description and Notes fields are required." })
+    } else if(description.length > 128){
+        res.status(400).json({ message: "Description needs to be less than 128 characters." })
+    } else {
+        next();
+    }
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require(express)
+const express = require("express");
 
 const projectRouter = require("./data/helpers/projectRouter");
 const actionRouter = require("./data/helpers/actionRouter");
 
 const server = express();
-server.use(expess.json());
+server.use(express.json());
 
 server.get("/", logger, (req, res) => {
     res.send(`<h2>Let's write some middleware!<h2/>`)
@@ -17,8 +17,8 @@ function logger(req, res, next) {
     console.log(req.url);
     console.log(
         `[${new Date().toISOString()}] ${req.method} to ${req.url}`
-      );
-    next();
-}
-
+        );
+        next();
+    }
+    
 module.exports = server;
